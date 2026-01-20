0b388c1376439876ff619147c2acbba0227de5c1
sutsutan <sutsutan9@gmail.com>
2026-01-20 21:38:35 +0700

    benerin error lagi

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,22 @@
-import { wayfinder } from '@laravel/vite-plugin-wayfinder';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
-import { defineConfig } from 'vite';
+import { wayfinder } from '@laravel/vite-plugin-wayfinder';
+
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            ssr: 'resources/js/ssr.tsx',
-            refresh: true,
-        }),
-        react({
-            babel: {
-                plugins: ['babel-plugin-react-compiler'],
-            },
-        }),
-        tailwindcss(),
-        wayfinder({
-            formVariants: true,
-        }),
-    ],
-    esbuild: {
-        jsx: 'automatic',
-    },
-});
+  plugins: [
+    laravel({
+      input: [
+        'resources/js/main.tsx',
+        'resources/js/index.css',
+        'resources/js/app.css',
+      ],
+      refresh: true,
+    }),
+    react(),
+    wayfinder({
+      formVariants: true,
+    }),
+  ],
+})
